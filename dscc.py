bc77833b5651ad4cc4a6649e502a722230bec079
Mahabubul Hasan <shourav6688@gmail.com>
2021-08-10 02:22:04 +0600

    translator API added

--- dscc.py
+++ dscc.py
@@ -1,18 +1,32 @@
 import xlrd
+from googletrans import Translator
+
 path = "./data/dscc_data.xlsx"
 inputWorkBook = xlrd.open_workbook(path)
 inputWorkSheet = inputWorkBook.sheet_by_index(0)
 json_data = []
+translator = Translator()
+# for i in range(1,inputWorkSheet.nrows):
+#     ward_data = {
+#         "area_name": {
+#             "en": inputWorkSheet.cell_value(i,1),
+#             "bn": ""
+#         },
+#         "city_corporation": "Dhaka South City Corporation",
+#         "city_corp_tag": "DSCC",
+#         "ward": str(int(inputWorkSheet.cell_value(i,0)))
+#     }
+#     json_data.append(ward_data)
 
-for i in range(1,inputWorkSheet.nrows):
+for i in range(1, inputWorkSheet.nrows):
     ward_data = {
         "area_name": {
-            "en": inputWorkSheet.cell_value(i,1),
-            "bn": ""
+            "en": inputWorkSheet.cell_value(i, 1),
+            "bn": translator.translate(inputWorkSheet.cell_value(i, 1), dest='bn')
         },
         "city_corporation": "Dhaka South City Corporation",
         "city_corp_tag": "DSCC",
-        "ward": int(inputWorkSheet.cell_value(i,0))
+        "ward": str(int(inputWorkSheet.cell_value(i, 0)))
     }
     json_data.append(ward_data)
 print(json_data)
---
+++ translator.py
@@ -0,0 +1,17 @@
+from googletrans import Translator
+import time
+start_time = time.time()
+translator = Translator()
+translation = translator.translate("Gulistan", dest='bn')
+translation1 = translator.translate("Mirpur", dest='bn')
+translation2 = translator.translate("Gopibagh", dest='bn')
+translation3 = translator.translate("Maniknagar Purbo (East)", dest='bn')
+translation4 = translator.translate("Kamlapur Dakkhin (South)", dest='bn')
+translation5 = translator.translate("Dakshin Mir Hazirbagh", dest='bn')
+print("Gulistan _>", translation.text)
+print("Mirpur ->", translation1.text)
+print("Gopibagh ->", translation2.text)
+print("Maniknagar Purbo (East) ->", translation3.text)
+print("Kamlapur Dakkhin (South) ->", translation4.text)
+print("Dakshin Mir Hazirbagh ->", translation5.text)
+print("--- %s seconds ---" % (time.time() - start_time))
